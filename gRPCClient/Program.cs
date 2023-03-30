5d980c047036da53725595e00ddfae8c3ae0f29f
Daanyaal Kapadia <daanyaalkapadia13@gmail.com>
2023-03-30 19:40:18 +0530

    Add project files.

---
+++ gRPCClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using gRPCDemo;
+using System.Diagnostics;
+
+namespace gRPCClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Stopwatch sp = new Stopwatch();
+            var channel = GrpcChannel.ForAddress("https://localhost:7206/");
+            var client = new Greeter.GreeterClient(channel);
+            sp.Start();
+            var reply = await client.SayHelloAsync(new HelloRequest() { Name = "Daanyaal" });
+            sp.Stop();
+            Console.WriteLine("Message received using gRPC :" + reply.Message);
+            Console.WriteLine("Elapse Milliseconds :" + sp.ElapsedMilliseconds);
+
+            var customerClient = new Customer.CustomerClient(channel);
+            var customer = await customerClient.GetCustomerInfoAsync(new CustomerLookUp() { Id = 1 });
+            Console.WriteLine("Customer Name " + customer.FirstName);
+
+            using(var cust = customerClient.GetAllCustomerInfo(new CustomerLookUp()))
+            {
+                while (await cust.ResponseStream.MoveNext())
+                {
+                    var curr = cust.ResponseStream.Current;
+                    Console.WriteLine("Customer Name " + curr.FirstName);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ gRPCDemo/Services/CustomerService.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Core;
+using gRPCDemo;
+
+namespace gRPCServer.Services
+{
+    public class CustomerService : Customer.CustomerBase
+    {
+        public override Task<CustomerDto> GetCustomerInfo(CustomerLookUp request, ServerCallContext context)
+        {
+            return Task.FromResult(
+                new CustomerDto() { Id = request.Id, FirstName = "DK", LastName = "DK" }
+                );
+        }
+        public async override Task GetAllCustomerInfo(CustomerLookUp request, IServerStreamWriter<CustomerDto> responseStream, ServerCallContext context)
+        {
+            List<CustomerDto> customerDtos = new List<CustomerDto>()
+            {
+                new CustomerDto() { Id = request.Id, FirstName = "DK", LastName = "DK" },
+                new CustomerDto() { Id = request.Id, FirstName = "VK", LastName = "VK" }
+            };
+            foreach(var cust in customerDtos)
+            {
+                await responseStream.WriteAsync(cust);
+            }
+        }
+    }
+}
